c94d448680796e13e280eccd1bb2494ac71e3a68
Juan Aparicio <apariciojuan30@gmail.com>
2023-01-26 13:30:07 -0300

    gem initialization

---
+++ lib/boosted.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "boosted/version"
+
+module Boosted
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/boosted_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Boosted do
+  it "has a version number" do
+    expect(Boosted::VERSION).not_to be nil
+  end
+end

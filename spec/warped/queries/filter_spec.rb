2d1bca5db8657a7eeafb5d1668b71e13fbd52eae
Juan Aparicio <apariciojuan30@gmail.com>
2024-04-05 14:19:49 -0300

    update specs

--- spec/warped/queries/filter_spec.rb
+++ spec/warped/queries/filter_spec.rb
@@ -46,7 +46,8 @@ RSpec.describe Warped::Queries::Filter do
         let(:relation) { "gt" }
 
         it "filters the scope by the given filter conditions" do
-          expect(scope).to receive(:where).with("? > ?", "id", 1)
+          expect(scope).to receive(:where).and_return(scope)
+          expect(scope).to receive(:not).with("id" => ...value)
 
           filter
         end
@@ -101,7 +102,7 @@ RSpec.describe Warped::Queries::Filter do
         let(:value) { [1, 2] }
 
         it "filters the scope by the given filter conditions" do
-          expect(scope).to receive(:where).with("id BETWEEN ? AND ?", 1, 2)
+          expect(scope).to receive(:where).with("id" => value.first..value.last)
 
           filter
         end
@@ -110,9 +111,19 @@ RSpec.describe Warped::Queries::Filter do
       context "when relation is starts_with" do
         let(:relation) { "starts_with" }
         let(:value) { "test" }
+        let(:arel_table) { double("ArelTable") }
+
+        before do
+          allow(scope).to receive(:where).with(anything).and_return(scope)
+          allow(scope).to receive(:arel_table).and_return(arel_table)
+          allow(arel_table).to receive(:[]).with(anything).and_return(arel_table)
+          allow(arel_table).to receive(:matches).with(anything)
+        end
 
         it "filters the scope by the given filter conditions" do
-          expect(scope).to receive(:where).with("id LIKE ?", "#{value}%")
+          expect(scope).to receive(:arel_table)
+          expect(arel_table).to receive(:[]).with(:id)
+          expect(arel_table).to receive(:matches).with("test%")
 
           filter
         end
@@ -121,9 +132,19 @@ RSpec.describe Warped::Queries::Filter do
       context "when relation is ends_with" do
         let(:relation) { "ends_with" }
         let(:value) { "test" }
+        let(:arel_table) { double("ArelTable") }
+
+        before do
+          allow(scope).to receive(:where).with(anything).and_return(scope)
+          allow(scope).to receive(:arel_table).and_return(arel_table)
+          allow(arel_table).to receive(:[]).with(anything).and_return(arel_table)
+          allow(arel_table).to receive(:matches).with(anything)
+        end
 
         it "filters the scope by the given filter conditions" do
-          expect(scope).to receive(:where).with("id LIKE ?", "%#{value}")
+          expect(scope).to receive(:arel_table)
+          expect(arel_table).to receive(:[]).with(:id)
+          expect(arel_table).to receive(:matches).with("%test")
 
           filter
         end
@@ -132,9 +153,19 @@ RSpec.describe Warped::Queries::Filter do
       context "when relation is contains" do
         let(:relation) { "contains" }
         let(:value) { "test" }
+        let(:arel_table) { double("ArelTable") }
+
+        before do
+          allow(scope).to receive(:where).with(anything).and_return(scope)
+          allow(scope).to receive(:arel_table).and_return(arel_table)
+          allow(arel_table).to receive(:[]).with(anything).and_return(arel_table)
+          allow(arel_table).to receive(:matches).with(anything)
+        end
 
         it "filters the scope by the given filter conditions" do
-          expect(scope).to receive(:where).with("id LIKE ?", "%#{value}%")
+          expect(scope).to receive(:arel_table)
+          expect(arel_table).to receive(:[]).with(:id)
+          expect(arel_table).to receive(:matches).with("%test%")
 
           filter
         end
